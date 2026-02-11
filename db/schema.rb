371da259dbf4b822c0fdade21c638c0062df74ce
ryusei <ryuuuuu6464@icloud.com>
2026-02-11 16:19:08 +0900

    feat(db): add devise users table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_11_040858) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_11_065739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,9 +56,27 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_11_040858) do
     t.index ["group_id"], name: "index_items_on_group_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.bigint "group_id"
+    t.string "name", null: false
+    t.boolean "is_guest", default: false, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["group_id", "name"], name: "index_users_on_group_id_and_name", unique: true
+    t.index ["group_id"], name: "index_users_on_group_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "categories", "groups", on_delete: :cascade
   add_foreign_key "inventories", "groups", on_delete: :cascade
   add_foreign_key "inventories", "items", on_delete: :cascade
   add_foreign_key "items", "categories", on_delete: :cascade
   add_foreign_key "items", "groups", on_delete: :cascade
+  add_foreign_key "users", "groups", on_delete: :cascade
 end
