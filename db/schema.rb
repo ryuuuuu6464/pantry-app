ac0560c1758ee3c89fdf739d26cedeef174e7c26
ryusei <ryuuuuu6464@icloud.com>
2026-02-11 13:06:18 +0900

    feat(db): add items table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_10_103116) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_11_032556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,5 +34,18 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_10_103116) do
     t.index ["is_guest", "expires_at"], name: "index_groups_on_is_guest_and_expires_at"
   end
 
+  create_table "items", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "category_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["group_id", "name"], name: "index_items_on_group_id_and_name", unique: true
+    t.index ["group_id"], name: "index_items_on_group_id"
+  end
+
   add_foreign_key "categories", "groups", on_delete: :cascade
+  add_foreign_key "items", "categories", on_delete: :cascade
+  add_foreign_key "items", "groups", on_delete: :cascade
 end
