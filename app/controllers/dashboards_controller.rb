029eec8bbb72a27f92c36787ebf3f06dcc2b3e53
ryusei <ryuuuuu6464@icloud.com>
2026-02-17 20:14:29 +0900

    feat(group): display users belong to group

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,7 @@ class DashboardsController < ApplicationController
   # 未ログインはログイン画面へ
   before_action :authenticate_user!
   def show
+    # current_userの所属グループのメンバーを取り出す
+    @group_users = current_user.group&.users
   end
 end
